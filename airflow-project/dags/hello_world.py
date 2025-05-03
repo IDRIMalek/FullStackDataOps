fdde8a995d761e54c86f7f9bc78f6bb87314a2ca
Malek IDRI <m.idri@apexenergies.fr>
2025-05-03 21:31:09 +0200

    Fonctionnement airflow standalone

--- airflow-project/dags/hello_world.py
+++ airflow-project/dags/hello_world.py
@@ -8,7 +8,7 @@ def say_hello():
 with DAG(
     dag_id='hello_world',
     start_date=datetime(2025, 1, 1),
-    schedule_interval='@daily',
+    schedule='@daily',
     catchup=False,
     tags=['example'],
 ) as dag:
